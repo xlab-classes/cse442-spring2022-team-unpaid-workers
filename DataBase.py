6477a298cc7aa9a9ad88d7ad8aa5d38596501afa
mbassam <mbassam@buffalo.edu>
2022-03-10 18:49:54 -0500

    added comment to the functions

--- DataBase.py
+++ DataBase.py
@@ -1,29 +1,33 @@
 import mysql.connector
 
+# this database will be storing sign in and sign up information #
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="ubcse442",
+    passwd="Shkaraot99@",
     database = "QuizHub"
 )
 mycursor = db.cursor()
 
+# create database for sign in and sign up username and password if not exist #
 mycursor.execute("CREATE DATABASE IF NOT EXISTS QuizHub")
 
+# this function will print the database usernames and password
 def print_user_table():
     mycursor.execute('SELECT * FROM user')
     for row in mycursor:
-        print(row)
+        print("db usernames: ", row)
+
     print("print successfully")
 
+# this is creating the table for the username and password storage #
 def creat_user_table():
-
-
     mycursor.execute("CREATE TABLE IF NOT EXISTS user (role VARCHAR(10),"
                       "username VARCHAR(20),"
                       "password VARCHAR(20),"
                       "_ID int PRIMARY key AUTO_INCREMENT)")
 
+# this function is going to insert the username and password into the table successfully
 def insert_user(tuple):
     try:
         creat_user_table()
@@ -36,12 +40,13 @@ def insert_user(tuple):
     except mysql.connector.Error:
         print("insert failed")
 
+# this function will check if the username exist in the database
 def username_is_not_exist(name):
     print(name)
     db = mysql.connector.connect(
         host="localhost",
         user="root",
-        passwd="ubcse442",
+        passwd="Shkaraot99@",
         database = "QuizHub"
     )
     mycursor = db.cursor()
@@ -51,6 +56,8 @@ def username_is_not_exist(name):
             return False
     return True
 
+# this function is going to check if the user entered the right username and password
+# in order to login to the website.
 def user_authentication(name, pw):
 
     try:
@@ -67,5 +74,6 @@ def user_authentication(name, pw):
     except mysql.connector.Error:
         print("check failed")
 
+# this function will be deleting the database.
 def delete_table():
     mycursor.execute("DROP TABLE user")
